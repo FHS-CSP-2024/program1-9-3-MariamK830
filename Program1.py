893f5042a1a5c39e6f1af982d4f0af5b231a102f
Mariam Khan <mariam.m.khan09@gmail.com>
2024-09-05 14:42:30 +0000

    Finished Program 1

--- Program1.py
+++ Program1.py
@@ -1,1 +1,33 @@
+print (":D")
+
+print("Toy Story           - 1995")
+print("A Bug's Life        - 1998")
+print("Monsters, Inc.      - 2001")
+print("Finding Nemo        - 2003")
+print("The Incredibles     - 2004")
+print("Cars                - 2006")
+print("Ratatouille         - 2007")
+print("WALL-E              - 2008")
+print("Up                  - 2009")
+print("Inside Out          - 2015")
+print("Onward              - 2020")
+
+print("Row, row, row your boat,\n\t" 
+"Gently down the stream.\n\t"
+"Merrily, merrily, merrily, merrily,\n\t"
+"Life is but a dream")
+
+print(4+3)
+print(4*3)
+print(1+1*3/4)
+
+print ((24*60)*365)
+
+print("I am super cool")
+print ("jeff says, 'you are not cool'")
+print ("jeff yelled, 'you are very annoying'")
+
+
+
+
 
